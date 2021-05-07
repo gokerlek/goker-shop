f5ed75b4a8fe4cef468e27a4993a29ef9ccbfeb9
Goker zafer <gokerlek@gmail.com>
2021-05-07 22:11:15 +0300

    add

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -20,7 +20,8 @@ export const Products: React.FC =()=> {
     },[])
 
     
-    return (<div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+    return (
+    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
     {items.map((item)=>(
     <ProductCard 
     id={item.id}
