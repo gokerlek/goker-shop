defdabc81d43235050f64c81e10c57d202e3d6c0
Goker zafer <gokerlek@gmail.com>
2021-05-07 12:04:03 +0300

    data fetch

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,7 +1,8 @@
+
 export const Search:React.FC =()=> {
     return ( 
     <div className="bg-white mt-4 text-sm border-transparent rounded-lg py-2 px-4 flex w-1/3 cursor-pointer">
-     <input className=" w-full m-0 p-0  border-none placeholder-gray-400 cursor-pointer" type="text" placeholder="Search your dream product..."/>
+     <input className=" w-full m-0 p-0  border-none focus:bo placeholder-gray-400 cursor-pointer" type="text" placeholder="Search your dream product..."/>
      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
 </svg>    
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,3 +1,25 @@
+import { useState, useEffect} from "react"
+
+interface item     {
+    id:Number,
+    title:String,
+    price:Number,
+    category:String,
+    description:String,
+    image:String
+  }
+  
 export const Products: React.FC =()=> {
-    return <p> products</p>
+
+    const [items, setItems] = useState<item[]>([])
+    useEffect (()=> {
+     fetch('https://fakestoreapi.com/products')
+     .then(res => res.json() as Promise<item[]>)
+     .then(json => setItems(json))
+    },[])
+
+    const data = items.map((item)=>(
+      item.category));
+
+    return <p>{data}</p>
 };
