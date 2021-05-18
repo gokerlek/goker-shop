e4ee8aac63aa4ebb5fad7e74e515cc8256c70005
Goker zafer <gokerlek@gmail.com>
2021-05-18 18:14:52 +0300

    delete item

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,15 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 
-interface item {
-     id: Number;
-     title: String;
-     price: Number;
-     category: String;
-     description: String;
-     image: String;
-}
-
 export const Product: React.FC = () => {
      const { id }: { id: string } = useParams();
      const [items, setItems] = useState({ title: "", id: "" });
@@ -21,7 +12,9 @@ export const Product: React.FC = () => {
      console.log(items.title);
 
      return (
-          <div className='grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6'>
+          <div
+               key={items.id}
+               className='grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6'>
                {items.title}
           </div>
      );
